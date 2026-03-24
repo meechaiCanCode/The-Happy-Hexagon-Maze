686310250012318187422fea5df4feb580aa76ac
Meechai <kerr.meechai@gmail.com>
2026-03-24 14:13:37 -0400

    Added test cases for HexGrid initialization

--- test/gridTest.cpp
+++ test/gridTest.cpp
@@ -1,7 +1,7 @@
 #include <catch2/catch_test_macros.hpp>
 #include "../src/HexGrid.hpp"
 
-TEST_CASE("Board Initializing", "[hexgrid]") {
+TEST_CASE("Init Just Size", "[hexgrid]") {
 	HexGrid test({10, 10});
 	int num1s = 0;
 	int num0s = 0;
@@ -14,15 +14,32 @@ TEST_CASE("Board Initializing", "[hexgrid]") {
 	REQUIRE(num0s == 35);
 	REQUIRE(num1s == 65);
 	REQUIRE(test.getStart() == std::pair<int, int>{0,0});
-/*
-	for (Test& test : tests) {
-		DYNAMIC_SECTION("Test: " << test.name) {
-			vector<int> sortedUfids;
-			for (const Student& s : test.insertionOrder) {
-				sortedUfids.push_back(s.ufid);
-			}
-			std::sort(sortedUfids.begin(), sortedUfids.end());
-			REQUIRE(sortedUfids == nodeVecToIntVec(test.bst.TraverseInorder()));
+}
+
+TEST_CASE("Init Size & %", "[hexgrid]") {
+	HexGrid test({10, 10}, 0.25);
+	int num1s = 0;
+	int num0s = 0;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if ((test.grid)[i][j] == 1) num1s++;
+			else num0s++;
 		}
-	}*/
+	}
+	REQUIRE(num0s == 25);
+	REQUIRE(num1s == 75);
+}
+
+TEST_CASE("Init All", "[hexgrid]") {
+	HexGrid test({10, 10}, 0.25, {1, 1}, {2, 1});
+	int num1s = 0;
+	int num0s = 0;
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if ((test.grid)[i][j] == 1) num1s++;
+			else num0s++;
+		}
+	}
+	REQUIRE(test.getStart() == std::pair<int, int>{1,1});
+	REQUIRE(test.getEnd() == std::pair<int, int>{2,1});
 }
